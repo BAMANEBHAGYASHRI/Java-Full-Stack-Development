9e524b9bde714cf0de1dfde1d44b864f00324853
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2025-03-20 15:24:18 +0530

    Patterns in java

---
+++ java/PATTERNS/pattern10.java
@@ -0,0 +1,28 @@
+class pattern10{
+public static void main(String x[]){
+for(int i =1 ; i<=5 ; i++){
+for(int j=1; j<=9 ; j++){
+if(j==6-i || j==4+i){
+ System.out.print("*");
+}
+else if (i==5 && j%2==1){
+System.out.print("*");
+}
+else{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+
+}
+}
+
+
+/*
+    *
+   *  *
+  *     *
+ *       *
+* * * * *
+*/
---
+++ java/PATTERNS/pattern7.java
@@ -0,0 +1,17 @@
+class pattern7{
+public static void main(String x[]){
+for(int i=1; i<=4; i++){
+for(int j=1; j<=7; j++){
+if(i<=j && j<=3+i){
+System.out.print("*");
+}
+else{
+System.out.print(" ");
+}
+}
+System.out.println();
+
+
+}
+
+}
