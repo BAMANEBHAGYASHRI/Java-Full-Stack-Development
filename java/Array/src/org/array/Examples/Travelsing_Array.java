f93bc3459f0e45e47e1db7784b555538bae72549
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2025-03-28 08:12:00 +0530

    Array

---
+++ java/Array/src/org/array/Examples/Travelsing_Array.java
@@ -0,0 +1,12 @@
+package org.array.Examples;
+
+public class Travelsing_Array {
+
+	public static void main(String[] args) {
+		int arr[] = {2,3,4,5,6};
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i] + "\t");
+		}
+	}
+
+}
