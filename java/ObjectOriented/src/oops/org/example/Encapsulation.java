e603fe6907b4f201c431e3bf1e0e5764d2b7fbd3
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2025-05-02 11:25:04 +0530

    Encapsulation

---
+++ java/ObjectOriented/src/oops/org/example/Encapsulation.java
@@ -0,0 +1,29 @@
+package oops.org.example;
+public class abuclhd{
+	private int id;
+	private String name;
+	private String email;
+	private int salary;
+	
+	public int getId() {
+		return id;
+	}
+	public int setID(int id) {
+		this.id=id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String setName(String name) {
+		this.name=name;
+	}
+}
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
